3a2b087a21eea313d15154cb906a50610ae18b93
Laurence Ho <bluegray.he@aol.com>
2019-03-31 19:13:26 +1300

    add api service

---
+++ client/src/services/api-service.ts
@@ -0,0 +1,62 @@
+import * as _ from 'lodash';
+
+export class ApiService {
+  perform(method: string, urlPath: string, requestBody: any, searchParams: any, formParams: any) {
+    const requestOptions: any = {};
+    requestOptions.mode = 'cors';
+    requestOptions.credentials = 'include';
+    requestOptions.cache = 'no-cache';
+    
+    const headers = new Headers({Accept: '*/*'});
+    this.authHeader(headers);
+    
+    // Construct request body
+    if (!_.isEmpty(formParams)) {
+      // Form submit
+      headers.append('Content-Type', 'application/x-www-form-urlencoded');
+      const formData = new FormData();
+      for (const formParam of Object.keys(formParams)) {
+        formData.append(formParam, formParams[ formParam ]);
+      }
+      requestOptions.body = formData;
+    } else if (!_.isEmpty(requestBody)) {
+      // JSON content
+      headers.append('Content-Type', 'application/json');
+      requestOptions.body = JSON.stringify(requestBody);
+    } else if (!_.isEmpty(searchParams)) {
+      const urlSearchParams = new URLSearchParams();
+      for (const param of Object.keys(searchParams)) {
+        urlSearchParams.append(param, searchParams[ param ]);
+      }
+      requestOptions.body = urlSearchParams;
+    }
+    
+    requestOptions.method = method.toUpperCase();
+    requestOptions.headers = headers;
+    
+    return fetch(urlPath, requestOptions)
+      .then(this.checkStatus)
+      .then(this.parseResponse)
+      .then(data => data);
+  }
+  
+  private checkStatus = (response: Response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      throw new Error(response.statusText);
+    }
+  }
+  
+  private parseResponse = (response: Response) => response.json();
+  
+  private authHeader = (headers: Headers) => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    
+    if (user && user.token) {
+      return headers.append('Authorization', `Bearer ${user.token}`);
+    } else {
+      return headers;
+    }
+  }
+}
