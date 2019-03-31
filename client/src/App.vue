ae46bc63da44142d3deed37de31abca6de62f439
Laurence Ho <bluegray.he@aol.com>
2019-03-31 19:12:51 +1300

    Tweak UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,42 +1,15 @@
 <template>
-  <el-container>
-    <el-header>
-      <top-bar :username="this.username"/>
-    </el-header>
-    <el-container>
-      <side-menu/>
-      <el-main>
-        <router-view/>
-      </el-main>
-    </el-container>
-  </el-container>
+  <router-view/>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
   import Component from 'vue-class-component'
-  import TopBar from './components/TopBar.vue';
-  import Dashboard from './components/Dashboard.vue';
-  import SideMenu from './components/SideMenu.vue';
 
-  @Component({
-    components: {SideMenu, TopBar, Dashboard}
-  })
+  @Component({})
   export default class App extends Vue {
-    username: string = '';
-
-    constructor() {
-      super();
-      this.username = 'Laurence Ho';
-    }
   }
 </script>
 
 <style scoped>
-  .el-header {
-    background-color: #545c64;
-    color: #fff;
-    line-height: 5rem;
-    padding: 0;
-  }
 </style>
---
+++ client/src/components/Layout.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-container>
+    <el-header>
+      <top-bar :username="this.username"/>
+    </el-header>
+    <el-container>
+      <side-menu/>
+      <el-main>
+        <dashboard/>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+  import TopBar from './TopBar.vue';
+  import Dashboard from './Dashboard.vue';
+  import SideMenu from './SideMenu.vue';
+
+  @Component({
+    components: {SideMenu, TopBar, Dashboard}
+  })
+  export default class Layout extends Vue {
+    username: string = '';
+
+    constructor() {
+      super();
+      this.username = 'Laurence Ho';
+    }
+  }
+</script>
+
+<style scoped>
+  .el-header {
+    background-color: #545c64;
+    color: #fff;
+    line-height: 5rem;
+    padding: 0;
+  }
+</style>
