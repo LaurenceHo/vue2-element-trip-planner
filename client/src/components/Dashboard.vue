54f6f8442892355fe452b922cb76f9c55c7fe3e4
Laurence Ho <bluegray.he@aol.com>
2019-03-31 16:56:30 +1300

    Use vuex

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -1,13 +1,28 @@
 <template>
-    <div class="main-content">
-      <div v-if="isLoading" :style="{width: '100%', textAlign: 'center'}">
-        <i class="el-icon-loading loading-spinner"></i>
-        Loading...
+  <div class="main-content">
+    <div v-if="this.$store.state.isLoading" :style="{width: '100%', textAlign: 'center'}">
+      <i class="el-icon-loading loading-spinner"></i>
+      Loading...
+    </div>
+    <div v-else>
+      <div v-if="this.$store.state.errorMessage">
+        <el-alert
+          type="error"
+          :title="this.$store.state.errorMessage"
+          :closable=false
+          show-icon>
+        </el-alert>
       </div>
       <div v-else>
-        <div v-if="trips.size === 0">You have no trip!</div>
+        <div v-if="this.$store.state.trips.size === 0">
+          <el-alert
+            title="You have no trip..."
+            type="info"
+            show-icon>
+          </el-alert>
+        </div>
         <div v-else>
-          <el-card class="box-card" v-for="trip in trips" :title="trip.name">
+          <el-card class="box-card" v-for="trip in this.$store.state.trips" :title="trip.name">
             <div slot="header" class="clearfix">
               <span>{{trip.name}}</span>
               <el-button style="float: right; padding: 3px 0" type="text">Detail</el-button>
@@ -21,25 +36,29 @@
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
   import Component from 'vue-class-component'
-  import { getAllTrips } from '../services/trip-service';
-  import { Trip } from '../models/trip';
+  import { TripService } from '../services/trip-service';
 
   @Component({})
 
   export default class Dashboard extends Vue {
-    trips: Trip[] = [];
-    isLoading: boolean = false;
+    tripService = new TripService();
 
     beforeMount() {
-      this.isLoading = true;
-      getAllTrips().then((trips: Trip[]) => {
-        this.trips = trips;
-        this.isLoading = false;
+      const requestBody = {};
+      this.$store.dispatch('isLoading', {isLoading: true});
+
+      this.tripService.getAllTrips(requestBody).then((result: any) => {
+        this.$store.dispatch('setTrips', {trips: result.result});
+        this.$store.dispatch('isLoading', {isLoading: false});
+      }).catch((error: any) => {
+        this.$store.dispatch('isLoading', {isLoading: false});
+        this.$store.dispatch('errorMessage', {errorMessage: error.message});
       });
     }
   }
@@ -65,6 +84,7 @@
     display: table;
     content: "";
   }
+
   .clearfix:after {
     clear: both
   }
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -5,16 +5,37 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    toggle: true
+    toggle: true,
+    isLoading: false,
+    errorMessage: '',
+    trips: []
   },
   mutations: {
     toggleSideBar(state: any) {
       state.toggle = !state.toggle;
+    },
+    isLoading(state, payload: any) {
+      state.isLoading = payload.isLoading;
+    },
+    errorMessage(state, payload: any) {
+      state.errorMessage = payload.errorMessage;
+    },
+    setTrips(state, payload: any) {
+      state.trips = payload.trips;
     }
   },
   actions: {
     toggleSideBar(context: any) {
       context.commit('toggleSideBar');
+    },
+    isLoading(context: any, payload: any) {
+      context.commit('isLoading', payload);
+    },
+    errorMessage(context: any, payload: any) {
+      context.commit('errorMessage', payload);
+    },
+    setTrips(context: any, payload: any) {
+      context.commit('setTrips', payload);
     }
   }
 });
