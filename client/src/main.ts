5ee8c04ee18824b8ff00db1731348a397bdfe86f
Laurence Ho <bluegray.he@aol.com>
2019-03-31 19:15:53 +1300

    Refactor trip service

--- client/src/main.ts
+++ client/src/main.ts
@@ -31,7 +31,7 @@ import Vue from 'vue';
 import 'whatwg-fetch';
 
 import App from './App.vue';
-import router from './router';
+import { router } from './router';
 import { store } from './store/store';
 import './style/style.css';
 
--- client/src/services/trip-service.ts
+++ client/src/services/trip-service.ts
@@ -1,29 +1,14 @@
+import { ApiService } from './api-service';
+
 const SERVER_URL = 'http://localhost:3000/api/';
 
-const checkStatus = (response: any) => {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  } else {
-    throw new Error(response.statusText);
+export class TripService {
+  apiService = new ApiService();
+  
+  getAllTrips(requestBody: any): any {
+    const searchParams = {};
+    const formParams = {};
+    
+    return this.apiService.perform('POST', `${SERVER_URL}trip`, requestBody, searchParams, formParams);
   }
-};
-
-const parseJSON = (response: any) => response.json();
-
-export const getAllTrips = () => {
-  return fetch(SERVER_URL + 'trip', {
-    method: 'POST'
-  }).then(checkStatus)
-    .then(parseJSON)
-    .then(data => data)
-    .catch(error => console.error('request failed', error));
-};
-
-export const getTripDetail = (id: number) => {
-  return fetch(SERVER_URL + `trip/${id}`, {
-    method: 'GET'
-  }).then(checkStatus)
-    .then(parseJSON)
-    .then(data => data)
-    .catch(error => console.error('request failed', error));
-};
+}
